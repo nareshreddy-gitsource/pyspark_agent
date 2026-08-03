d48d47f971117763e17ac7ce1a4bd277735b9bfa
nareshreddy-gitsource <nareshreddy.cde@gmail.com>
2026-08-03 12:25:06 +0530

    Add files via upload

--- examples/sample_query.sql
+++ examples/sample_query.sql
@@ -8,3 +8,14 @@ WHERE status = 'completed'
 GROUP BY customer_id, region
 HAVING SUM(amount) > 500
 ORDER BY total_amount DESC;
+
+with CTE_rank as
+(SELECT
+    customer_id,
+    region,
+    DENSE_RANK() OVER (PARTITION BY department ORDER BY salary DESC) AS drnk
+FROM employee
+)
+
+SELECT * FROM CTE_rank
+WHERE drnk = 2
